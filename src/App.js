995e79246acc3a705fc10ddc10572db319945167
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-09-11 13:10:54 +0200

    Add AboutMe page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import NavBar from './components/NavBar';
 import HomePage from './pages/HomePage';
 import PostDetail from './pages/PostDetail';
-
+import AboutMe from './pages/AboutMe'
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Routes>
         <Route path='/' element={<HomePage/>}></Route>
+        <Route path='about-me' element={<AboutMe/>}></Route>
         <Route path='/posts/:id' element={<PostDetail/>}></Route>
       </Routes>
 
---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AboutMe() {
+  return (
+    <div>
+        <h2>Webbutveckling med CMS examinationsuppgift - Jesper Grankvist</h2>
+        <p>Headless CMS med React som frontend och Wordpress.com som backend</p>
+    </div>
+  )
+}
